452ea88f167a0b4ee309b145eba6348c4a22a072
Govind Mantri <mantri.govind@hotmail.com>
2014-01-28 23:08:59 +0530

    new files added (Initial Commit)

---
+++ config.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * This file defines the 'LinkedIn' class. This class is designed to be a 
+ * simple, stand-alone implementation of the LinkedIn API functions.
+ * 
+ * COPYRIGHT:
+ *   
+ * Copyright (C) 2014, Govind Mantri
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.  
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.  
+ *
+ */
+?>
+<?php
+ 
+function oauth_session_exists() {
+  if((is_array($_SESSION)) && (array_key_exists('oauth', $_SESSION))) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
+
+try {
+  // include the LinkedIn class
+  require_once('linkedin.class.php');
+  
+  // start the session
+  if(!session_start()) {
+    throw new LinkedInException('This script requires session support, which appears to be disabled according to session_start().');
+  }
+  
+  // display constants
+  $API_CONFIG = array(
+    'appKey'       => '754v9mftcdaqhh',
+	  'appSecret'    => 'PNevjkV9kQ826mB0',
+	  'callbackUrl'  => NULL 
+  );
+  define('DEMO_GROUP', '4010474');
+  define('DEMO_GROUP_NAME', 'Simple LI Demo');
+  define('PORT_HTTP', '80');
+  define('PORT_HTTP_SSL', '443');
+
+  // set index
+  $_REQUEST[LINKEDIN::_GET_TYPE] = (isset($_REQUEST[LINKEDIN::_GET_TYPE])) ? $_REQUEST[LINKEDIN::_GET_TYPE] : '';
+  switch($_REQUEST[LINKEDIN::_GET_TYPE]) {
+    case 'initiate':
+      /**
+       * Handle user initiated LinkedIn connection, create the LinkedIn object.
+       */
+        
+      // check for the correct http protocol (i.e. is this script being served via http or https)
+      if($_SERVER['HTTPS'] == 'on') {
+        $protocol = 'https';
+      } else {
+        $protocol = 'http';
+      }
+      
+      // set the callback url
+      $API_CONFIG['callbackUrl'] = $protocol . '://' . $_SERVER['SERVER_NAME'] . ((($_SERVER['SERVER_PORT'] != PORT_HTTP) || ($_SERVER['SERVER_PORT'] != PORT_HTTP_SSL)) ? ':' . $_SERVER['SERVER_PORT'] : '') . $_SERVER['PHP_SELF'] . '?' . LINKEDIN::_GET_TYPE . '=initiate&' . LINKEDIN::_GET_RESPONSE . '=1';
+      $OBJ_linkedin = new LinkedIn($API_CONFIG);
+      
+      // check for response from LinkedIn
+      $_GET[LINKEDIN::_GET_RESPONSE] = (isset($_GET[LINKEDIN::_GET_RESPONSE])) ? $_GET[LINKEDIN::_GET_RESPONSE] : '';
+      if(!$_GET[LINKEDIN::_GET_RESPONSE]) {
+        // LinkedIn hasn't sent us a response, the user is initiating the connection
+        
+        // send a request for a LinkedIn access token
+        $response = $OBJ_linkedin->retrieveTokenRequest();
+        if($response['success'] === TRUE) {
+          // store the request token
+          $_SESSION['oauth']['linkedin']['request'] = $response['linkedin'];
+          
+          // redirect the user to the LinkedIn authentication/authorisation page to initiate validation.
+          header('Location: ' . LINKEDIN::_URL_AUTH . $response['linkedin']['oauth_token']);
+        } else {
+          // bad token request
+          echo "Request token retrieval failed:<br /><br />RESPONSE:<br /><br /><pre>" . print_r($response, TRUE) . "</pre><br /><br />LINKEDIN OBJ:<br /><br /><pre>" . print_r($OBJ_linkedin, TRUE) . "</pre>";
+        }
+      } else {
+        // LinkedIn has sent a response, user has granted permission, take the temp access token, the user's secret and the verifier to request the user's real secret key
+        $response = $OBJ_linkedin->retrieveTokenAccess($_SESSION['oauth']['linkedin']['request']['oauth_token'], $_SESSION['oauth']['linkedin']['request']['oauth_token_secret'], $_GET['oauth_verifier']);
+        if($response['success'] === TRUE) {
+          // the request went through without an error, gather user's 'access' tokens
+          $_SESSION['oauth']['linkedin']['access'] = $response['linkedin'];
+          
+          // set the user as authorized for future quick reference
+          $_SESSION['oauth']['linkedin']['authorized'] = TRUE;
+            
+          // redirect the user back to the demo page
+          header('Location: ' . $_SERVER['PHP_SELF']);
+        } else {
+          // bad token access
+          echo "Access token retrieval failed:<br /><br />RESPONSE:<br /><br /><pre>" . print_r($response, TRUE) . "</pre><br /><br />LINKEDIN OBJ:<br /><br /><pre>" . print_r($OBJ_linkedin, TRUE) . "</pre>";
+        }
+      }
+      break;
+
+    case 'revoke':
+      /**
+       * Handle authorization revocation.
+       */
+                    
+      // check the session
+      if(!oauth_session_exists()) {
+        throw new LinkedInException('This script requires session support, which doesn\'t appear to be working correctly.');
+      }
+      
+      $OBJ_linkedin = new LinkedIn($API_CONFIG);
+      $OBJ_linkedin->setTokenAccess($_SESSION['oauth']['linkedin']['access']);
+      $response = $OBJ_linkedin->revoke();
+      if($response['success'] === TRUE) {
+        // revocation successful, clear session
+        session_unset();
+        $_SESSION = array();
+        if(session_destroy()) {
+          // session destroyed
+          header('Location: ' . $_SERVER['PHP_SELF']);
+        } else {
+          // session not destroyed
+          echo "Error clearing user's session";
+        }
+      } else {
+        // revocation failed
+        echo "Error revoking user's token:<br /><br />RESPONSE:<br /><br /><pre>" . print_r($response, TRUE) . "</pre><br /><br />LINKEDIN OBJ:<br /><br /><pre>" . print_r($OBJ_linkedin, TRUE) . "</pre>";
+      }
+      break;
+    default:
+      // nothing being passed back, display demo page
+      
+      // check PHP version
+      if(version_compare(PHP_VERSION, '5.0.0', '<')) {
+        throw new LinkedInException('You must be running version 5.x or greater of PHP to use this library.'); 
+      } 
+      
+      // check for cURL
+      if(extension_loaded('curl')) {
+        $curl_version = curl_version();
+        $curl_version = $curl_version['version'];
+      } else {
+        throw new LinkedInException('You must load the cURL extension to use this library.'); 
+      }
+     
+      break;
+  }
+} catch(LinkedInException $e) {
+  // exception raised by library call
+  echo $e->getMessage();
+}
+
+?>
